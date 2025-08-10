6d8da0ad83f7af569b67c1e803df3c9602afba4a
YourGitHubName <your@email.com>
2025-08-10 21:35:37 +0530

    Initial commit of Sashacoin project structure

---
+++ launch_sashacoin.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "Opening CoinFactory/Sashacoin..."
+ls -la ~/CoinFactory/Sashacoin
+echo
+echo "Quick view of key files:"
+echo " - CoinDump: ~/CoinFactory/Sashacoin/CoinDump.txt"
+echo " - Influencer list: ~/CoinFactory/Sashacoin/Celebrity_Outreach_Kit/Influencer_List.csv"
+echo " - Launch checklist: ~/CoinFactory/Sashacoin/Launch/Launch_Day_Checklist.txt"
